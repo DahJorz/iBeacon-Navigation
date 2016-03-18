b7f50b23633b6d7ef8cb2f7a2a7fadea694e7d83
DahJorz <jordy630@hotmail.com>
2016-03-18 11:06:22 +0100

    Jordy: Changed back to Visio beacons + added proximity label

--- iBeaconTest1/iBeaconTest1/SecondViewController.swift
+++ iBeaconTest1/iBeaconTest1/SecondViewController.swift
@@ -20,10 +20,10 @@ class SecondViewController: UIViewController, CLLocationManagerDelegate {
         36783: UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1),    //KILz
         22239: UIColor(red: 0/255, green: 0/255, blue: 255/255, alpha: 1),    //KT80
         27120: UIColor(red: 154/255, green: 65/255, blue: 183/255, alpha: 1), //vVmc
-        2376:  UIColor(red: 33/255, green: 183/255, blue: 55/255, alpha: 1),  //zr3i
-        54482: UIColor(red: 0/255, green: 255/255, blue:255/255, alpha: 1),    //Chfq
-        55620: UIColor(red: 255/255, green: 255/255, blue:0/255, alpha: 1),    //WPYp
-        41997: UIColor(red: 128/255, green: 0/255, blue:0/255, alpha: 1),      //B5nr
+        //2376:  UIColor(red: 33/255, green: 183/255, blue: 55/255, alpha: 1),  //zr3i
+        //54482: UIColor(red: 0/255, green: 255/255, blue:255/255, alpha: 1),    //Chfq
+        //55620: UIColor(red: 255/255, green: 255/255, blue:0/255, alpha: 1),    //WPYp
+        //41997: UIColor(red: 128/255, green: 0/255, blue:0/255, alpha: 1),      //B5nr
         3992: UIColor(red: 249/255, green: 163/255, blue:11/255, alpha: 1),    //XX1f
         24799: UIColor(red: 0/255, green: 128/255, blue:128/255, alpha: 1)    //icvL
         
@@ -32,10 +32,10 @@ class SecondViewController: UIViewController, CLLocationManagerDelegate {
         36783: "KILz -> Links", //KILZ
         22239: "KT80 -> Rechts", //KT80
         27120: "vVmc -> Links", //vVmc
-        2376:  "zr3i", //zr3i
-        54482: "Chfq", //Chfq
-        55620: "WPYp", //WPYp
-        41997: "B5nr", //B5nr
+        //2376:  "zr3i", //zr3i
+        //54482: "Chfq", //Chfq
+        //55620: "WPYp", //WPYp
+        //41997: "B5nr", //B5nr
         3992:  "XX1f -> Links", //XX1f
         24799: "icvL -> Links"  //icvL
     ]
@@ -66,16 +66,16 @@ class SecondViewController: UIViewController, CLLocationManagerDelegate {
             let closestBeacon = knownBeacons[0] as CLBeacon
             self.view.backgroundColor = self.colors[closestBeacon.minor.integerValue]
             //label.text = self.names[closestBeacon.minor.integerValue]
-            let label = UILabel(frame: CGRectMake(0, 0, 200, 21))
-            label.center = CGPointMake(160, 284)
-            label.textAlignment = NSTextAlignment.Center
-            label.text = self.names[closestBeacon.minor.integerValue]
+            //let label = UILabel(frame: CGRectMake(0, 0, 200, 21))
+            //label.center = CGPointMake(160, 284)
+            //label.textAlignment = NSTextAlignment.Center
+            //label.text = self.names[closestBeacon.minor.integerValue]
             instruction.text = self.names[closestBeacon.minor.integerValue]
             //label.textColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
-            label.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+            //label.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
             
             //label.removeFromSuperview()
-            self.view.addSubview(label)
+            //self.view.addSubview(label)
             //self.subview.label.text = ""
         }
     }
